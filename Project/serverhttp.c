ddb503648010f9e2d536e7095493c17e4c460b69
Nur <2020975359@isiswa.uitm.edu.my>
2020-12-20 09:48:25 +0000

    HTTP Server

--- Project/serverhttp.c
+++ Project/serverhttp.c
@@ -1,13 +1,14 @@
-#include"stdio.h"
-#include"stdlib.h"
-#include"sys/types.h"
-#include"sys/socket.h"
-#include"string.h"
-#include"netinet/in.h"
-#include"time.h"
-#include"dirent.h"
-#include"netdb.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <dirent.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
 #define BUF_SIZE 1024
 #define CLADDR_LEN 100
 
