8bb87d2261f0bbae44f75a7e1548d9915a594b85
Nur <2020975359@isiswa.uitm.edu.my>
2020-12-20 12:43:05 +0000

    Client Update

--- client.c
+++ client.c
@@ -13,7 +13,7 @@ int main(int argc, char const *argv[])
 {
     int sock = 0; long valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    char *hello = "Connected to client";
     char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -39,7 +39,7 @@ int main(int argc, char const *argv[])
         return -1;
     }
     send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    printf("Request is received\n");
     valread = read( sock , buffer, 1024);
     printf("%s\n",buffer );
     return 0;
